e215abc52cf3666f077039e6db90711c6dc6a63d
Paul Evans <decretist@gmail.com>
2020-05-28 17:19:08 -0700

    Finalize first plot

--- final.R
+++ final.R
@@ -1,14 +1,24 @@
+#
 # Paul Evans (10evans@cua.edu)
 # 28 May 2020
 #
 setwd("~/Work/PCA")
 library(stylo)
 #
+# 4-way comparison of case statements, 1st-recension, de Pen.,
+# and 2nd-recension dicta:
+#
+# Gratian0.txt (vulgate case statements)
+# Gratian1.txt (1st-recension dicta excluding de Pen.)
+# dePen.txt    (1st- and 2nd-recension dicta from de Pen.)
+# Gratian2.txt (2nd-recension dicta excluding de Pen.)
+#
 files.to.analyze <- c("Gratian0.txt", "Gratian1.txt", "dePen.txt", "Gratian2.txt")
 writeLines(files.to.analyze, "files_to_analyze.txt")
 #
 stylo.results = stylo(
-  gui = TRUE,
+  gui = FALSE,
+  corpus.dir = "corpora/final_xque",
   corpus.lang = "Latin.corr",
   mfw.min = 52, mfw.max = 52,
   mfw.list.cutoff = 240,
@@ -18,8 +28,11 @@ stylo.results = stylo(
   analysis.type = "PCR",
   sampling = "normal.sampling",
   sample.size = 1200,
-  # colors.on.graphs = "greyscale",
-  pca.visual.flavour = "symbols"
+  write.jpg.file = TRUE,
+  pca.visual.flavour = "symbols",
+  custom.graph.title = "4-way",
+  custom.graph.filename = "JPGs/4-way_PCA_52_MFWs"
 )
 # summary(stylo.results)
 print(stylo.results$features.actually.used)
+
